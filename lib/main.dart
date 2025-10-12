10e893a44277509714962dd956e623a90e0e4356
HuynhThanhNhuanB2205999 <nhuanb2205999@student.ctu.edu.vn>
2025-10-12 12:59:44 +0700

    Sets up main(), MyApp, MultiProvider for MovieProvider and FavoritesProvider, applies the dark theme, and initializes navigation with MaterialApp.router.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-// (Code của main.dart đã được gửi)
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'providers/favorites_provider.dart';
-import 'providers/movie_provider.dart';
-import 'router/app_router.dart';
+import '../providers/favorites_provider.dart';
+import '../providers/movie_provider.dart';
+import '../router/app_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Sử dụng MultiProvider để cung cấp các Provider cho toàn bộ cây Widget
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => MovieProvider()),
-        // Khởi tạo FavoritesProvider để tải dữ liệu từ database khi app khởi động
         ChangeNotifierProvider(create: (_) => FavoritesProvider()),
       ],
       child: MaterialApp.router(
@@ -26,20 +23,13 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Movie App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
-          brightness: Brightness.dark, // Sử dụng theme tối
+          brightness: Brightness.dark,
           scaffoldBackgroundColor: Colors.black,
           appBarTheme: const AppBarTheme(
             backgroundColor: Colors.black,
             elevation: 0,
           ),
-          // Thiết lập màu cho BottomNavigationBar
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Colors.black,
-            selectedItemColor: Colors.blue, // Màu icon khi được chọn
-            unselectedItemColor: Colors.white70, // Màu icon khi không được chọn
-          ),
         ),
-        // Sử dụng GoRouter để quản lý điều hướng
         routerConfig: AppRouter.router,
       ),
     );
