fda1f1a35f96f27e632116891fb152992249be18
Gabu1909 <kinepone2004@gmail.com>
2025-10-12 13:00:11 +0700

    Update movie_detail_screen.dart

--- lib/screens/movie_detail_screen.dart
+++ lib/screens/movie_detail_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/movie_detail_screen.dart
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
